a83bdcfcda20ee0909e42b2e2e6f1c01d2d62854
pengmaofang <2559288989@qq.com>
2023-04-04 15:31:40 +0800

    666

--- src/router/index.js
+++ src/router/index.js
@@ -1,80 +1,10 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import {getItem} from "@/utils/storage";
-
 
 const routes = [
     {
-        path: '/login',
-        name: 'login',
-        component: () => import('../views/user/LoginPage')
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: () => import('../views/user/RegisterPage')
-    },
-    {
         path: '/',
+        name: 'index',
         component: () => import('../views/user/LoginPage')
-    },
-    {
-        path: "/ProductDetail",
-        name: "ProductDetail",
-        component: () => import('../views/font-page/ProductDetail'),
-    },{
-        path: "/OrderDetail",
-        name: "OrderDetail",
-        component: () => import('../views/font-page/OrderDetail'),
-    },
-
-
-    {
-        path: '/FontPage',
-        component: () => import('../views/layout/FontPage'),
-        children: [
-            {
-                path: "/IndexPage",
-                name: "IndexPage",
-                component: () => import('../views/font-page/IndexPage'),
-            },
-            {
-                path: "/SearchShop",
-                name: "SearchShop",
-                component: () => import('../views/font-page/SearchShop.vue'),
-            },
-            {
-                path: "/ShoppingCar",
-                name: "ShoppingCar",
-                component: () => import('../views/font-page/ShoppingCar'),
-            },
-            {
-                path: "/PersonalCenter",
-                name: "PersonalCenter",
-                component: () => import('../views/font-page/PersonalCenter'),
-            },
-            {
-                path: "/ProductDetail",
-                name: "ProductDetail",
-                component: () => import('../views/font-page/ProductDetail.vue'),
-            },
-        ]
-    }
-    , {
-        path: '/EndPage',
-        name: 'EndPage',
-        component: () => import('../views/layout/EndPage'),
-        children: [
-            {
-                path: "/UserManagement",
-                name: "UserManagement",
-                component: () => import('../views/end-page/UserManagement'),
-            },{
-                path: "/GoodsManagement",
-                name: "GoodsManagement",
-                component: () => import('../views/end-page/GoodsManagement'),
-            }
-
-        ]
     }
 ]
 
@@ -82,20 +12,6 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
-})
-
-router.beforeEach((to, from, next) => {
-
-
-    if (to.href === '/login' || to.href === '/register') {
-        next()
-    } else {
-        if (to.href !== '/login' && getItem("TOKEN_INFO_KEY") === null) {
-            next('/login')
-        } else {
-            next()
-        }
-    }
 })
 
 
--- src/views/user/LoginPage.vue
+++ src/views/user/LoginPage.vue
@@ -3,7 +3,7 @@
 
     <div class="div1">
       <div style="text-align: left;color:#ffffff;height: 80px;font-size: 42px;line-height: 80px;padding-left: 30px;font-weight: bold">
-        农产品推荐系统
+        基于springboot+vue实现的开机检测
       </div>
       <div>
         <div class="div2">
@@ -11,44 +11,12 @@
               <el-card>
                 <template #header>
                   <div style="font-size: 26px">
-                    请登录
+                    开机检测
                   </div>
                 </template>
-                <el-row justify="center">
-                  <el-col :span="20">
-                    <el-form>
-                      <el-form-item>
-                        <el-input  :prefix-icon="User" v-model="user.username" placeholder="请输入用户名"></el-input>
-                      </el-form-item>
-                      <el-form-item>
-                        <el-input  type="password" show-password :prefix-icon="Lock" v-model="user.password" placeholder="请输入密码"></el-input>
-                      </el-form-item>
-                      <el-row :gutter="10">
-                        <el-col :span="15">
-                          <el-form-item>
-                            <el-input  :prefix-icon="View"  v-model="user.userCode" placeholder="请输入验证码"></el-input>
-                          </el-form-item>
-                        </el-col>
-                        <el-col :span="9">
-                          <el-form-item>
-                            <img :src="user.code" @click="changeCapHandler">
-                          </el-form-item>
-                        </el-col>
-                      </el-row>
-                    </el-form>
-                    <div>
-                      <el-row >
-                        <el-col :span="12">
-                          <el-link  type="primary" :underline="false" @click="registerHandler" >注册</el-link>
-                        </el-col>
-                        <el-col style="text-align: right" :span="12">
-                          <el-link  type="danger" :underline="false">忘记密码?</el-link>
-                        </el-col>
-                      </el-row>
-                    </div>
-                    <el-button  @keyup.down.enter="alert('123')" @click="loginHandler" style="background:#247ff2;color:#ffffff; width: 100%;margin-top: 10px">登录</el-button>
-                  </el-col>
-                </el-row>
+                <div>
+                <el-button type="primary" @click="checkHandle">点击检测</el-button>
+                </div>
               </el-card>
           </div>
         </div>
@@ -60,73 +28,29 @@
 
 <script>
 
-import {login, systemCaptcha} from "@/api/api";
-import {useStore} from 'vuex'
-import router from "@/router";
-import {User,Lock,View} from "@element-plus/icons-vue";
 
+import { ElMessageBox} from "element-plus";
 
 export default {
   name: "LoginPage",
-  computed: {
-    User() {
-      return User
-    },
-    Lock() {
-      return Lock
-    },
-    View() {
-      return View
-    }
-  },
 
-  setup() {
-    return {
-      store: useStore()
-    }
-  },
+
 
   data() {
     return {
-      user: {
-        username: '',
-        password: '',
-        code:'',
-        uuid:'',
-        userCode:'',
-      }
+      inputAge: '',
+      result: ''
     }
   },
-  components: {},
+
   methods: {
-
-    registerHandler(){
-      router.push({path: '/register'})
-    },
-
-    changeCapHandler(){
-      systemCaptcha().then((resp) => {
-        let data= resp.data.data
-        this.user.code=data.code
-        this.user.uuid=data.uuid
+    checkHandle(){
+      ElMessageBox.alert('您的电脑已开机', '提示', {
+        confirmButtonText: '确定',
       })
-    },
-
-    loginHandler() {
-      login(this.user).then((resp => {
-        if (resp.data.code === 200) {
-          this.store.commit('setUser', resp.data.data)
-          router.push({path: '/IndexPage'})
-        }
-      }))
-    },
+    }
   },
-  mounted() {
 
-    systemCaptcha().then((resp) => {
-      this.user = resp.data.data;
-    })
-  },
 
 }
 </script>
