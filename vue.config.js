b958d5598f77aecdc6becc08daf57e78babc97cd
pengmaofang <2559288989@qq.com>
2023-05-06 08:49:04 +0800

    666

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     open: true,
     host: 'localhost',
-    port: 8080,
+    port: 8081,
     https: false,
     //以上的ip和端口是我们本机的;下面为需要跨域的
     proxy: { //配置跨域
