b97f27bd6cf3b40d9f56df2819797b62dac15bd4
pengmaofang <2559288989@qq.com>
2023-05-15 00:25:44 +0800

    666

--- src/views/end-page/CapabilityAssessment.vue
+++ src/views/end-page/CapabilityAssessment.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
+    <el-row :gutter="10">
+      <el-col :span="11">
+        <div style="width: 100%;height: 600px" id="dkqj1"></div>
+      </el-col>
 
+      <el-col :span="11">
+        <div style="width: 100%;height: 600px" id="dkqj2"></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
